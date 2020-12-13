f90957eebc2b59afbeed8b8276104704bc21e2af
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2020-12-13 14:21:13 +0100

    Support requests - Add authentication to srq routes

--- src/routers/srq.ts
+++ src/routers/srq.ts
@@ -4,8 +4,16 @@ import * as supportRequestController from "../controllers/srqController";
 const router = express.Router();
 
 router.get("/srq", isAuth, supportRequestController.getAllSupportRequests);
-router.post("/srq", supportRequestController.postAddSupportRequest);
-router.patch("/srq/:srqId", supportRequestController.updateSupportRequestById);
-router.delete("/srq/:srqId", supportRequestController.deleteSupportRequestById);
+router.post("/srq", isAuth, supportRequestController.postAddSupportRequest);
+router.patch(
+  "/srq/:srqId",
+  isAuth,
+  supportRequestController.updateSupportRequestById
+);
+router.delete(
+  "/srq/:srqId",
+  isAuth,
+  supportRequestController.deleteSupportRequestById
+);
 
 export default router;
