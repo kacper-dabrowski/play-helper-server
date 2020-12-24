91d3dd2345b9a9e75f980a5437e6de36efd26030
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2020-12-24 08:15:14 +0100

    CORS - Add CORS policy middleware

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import bodyParser from "body-parser";
 import express from "express";
 import supportRequestRouter from "./routers/srq";
+import cors from "cors";
 
 require("./db/mongoose");
 
 const app = express();
+const corsWithOptions = cors({});
+
 const PORT = process.env.PORT || 3000;
 
+app.use(corsWithOptions);
 app.use(bodyParser.json());
 
 app.use(supportRequestRouter);
