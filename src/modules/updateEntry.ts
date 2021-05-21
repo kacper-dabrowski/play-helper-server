93d644f259df35888ab4f9f4a21ff5de43d6f5ee
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-05-21 22:52:26 +0200

    Add tests for updateEntry

--- src/modules/updateEntry.ts
+++ src/modules/updateEntry.ts
@@ -1,6 +1,12 @@
 type objectEntry<T> = [string & keyof T, T[string & keyof T]];
 
 const updateEntry = <T>(object: T, updates: Partial<T>, updatableFields: Array<string>): T => {
+    const updateKeys = Object.keys(updates);
+
+    if (updateKeys.length === 0) {
+        return object;
+    }
+
     Object.entries(updates).forEach((entry) => {
         const [key, value] = entry as objectEntry<T>;
         if (updatableFields.includes(key) && value) {
