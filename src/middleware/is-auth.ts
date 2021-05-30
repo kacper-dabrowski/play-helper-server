7508fa0f22773378eaa0628de2c36f6f7a939d02
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-05-31 00:04:27 +0200

    Tests - Add tests for login flow

--- src/middleware/is-auth.ts
+++ src/middleware/is-auth.ts
@@ -1,20 +1,14 @@
-import jwt from 'jsonwebtoken';
-import { User } from '../models/User';
+import verifyToken from '../modules/auth/verifyToken';
 import { MiddlewareFn } from './Middleware';
 import { routeWrapper } from './routeWrapper';
-import Errors from '../modules/errors';
 
 const isAuth: MiddlewareFn = routeWrapper((req, res, next) => {
     try {
         const token = req.get('Authorization')?.split(' ')[1];
-        if (!token || !process.env.JWT_KEY) {
-            throw new Errors.BadRequestError('Authentication failed');
-        }
-        const decodedToken = jwt.verify(token, process.env.JWT_KEY) as User;
-        if (!decodedToken) {
-            Errors.NoGrantsError();
-        }
+
+        const decodedToken = verifyToken(token);
         req.userId = decodedToken.userId;
+
         next();
     } catch (error) {
         next(error);
