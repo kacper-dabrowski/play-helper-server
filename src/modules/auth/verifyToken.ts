c130eec123a3838b19337e3887ef9112d2755442
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-06-19 12:28:11 +0200

    Fix authentication error throwing

--- src/modules/auth/verifyToken.ts
+++ src/modules/auth/verifyToken.ts
@@ -3,14 +3,21 @@ import { User } from '../../models/User';
 import Errors from '../../modules/errors';
 
 const verifyToken = (token?: string): User => {
-    if (!token || !process.env.JWT_KEY) {
+    try {
+        if (!token || !process.env.JWT_KEY) {
+            throw new Errors.BadRequestError('Authentication failed');
+        }
+
+        const decodedToken = jwt.verify(token, process.env.JWT_KEY) as User;
+
+        if (!decodedToken) {
+            throw new Errors.NoGrantsError();
+        }
+
+        return decodedToken;
+    } catch (error) {
         throw new Errors.BadRequestError('Authentication failed');
     }
-    const decodedToken = jwt.verify(token, process.env.JWT_KEY) as User;
-    if (!decodedToken) {
-        throw new Errors.NoGrantsError();
-    }
-    return decodedToken;
 };
 
 export default verifyToken;
