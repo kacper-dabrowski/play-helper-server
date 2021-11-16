bf9570b5ffeff03fc7cf099ee11099dba8985cc3
kacdab003 <46220934+kacdab003@users.noreply.github.com>
2021-11-17 00:00:05 +0100

    test

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import supportRequestRouter from './routers/srq';
-import authRouter from './routers/auth';
+import supportRequestRouter from './routers/srq/srq';
+import authRouter from './routers/auth/auth';
 import cors from 'cors';
-import errorHandler from './utils/errors/errorHandler';
-import notFoundRouter from './routers/notFound';
-import solutionRouter from './routers/solution';
-import healthCheckRouter from './routers/healthCheck';
+import errorHandler from './modules/errors/errorHandler';
+import settingsRouter from './routers/user/settings';
+import notFoundRouter from './routers/notFound/notFound';
+import solutionRouter from './routers/solution/solution';
+import healthCheckRouter from './routers/healthCheck/healthCheck';
+import connectToDb from './db/mongoose';
 
 require('./db/mongoose');
 
 const app = express();
 const corsWithOptions = cors({});
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(corsWithOptions);
 app.use(healthCheckRouter);
@@ -21,9 +23,21 @@ app.use(bodyParser.json());
 app.use(authRouter);
 app.use(supportRequestRouter);
 app.use(solutionRouter);
+app.use(settingsRouter);
 app.use('*', notFoundRouter);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log('App is running on ' + PORT);
-});
+connectToDb()
+    .then(() => {
+        if (process.env.NODE_ENV !== 'test') {
+            app.listen(PORT, () => {
+                console.log('App is running on ' + PORT);
+            });
+        }
+    })
+    .catch((error) => {
+        console.error('Failed to connect to the DB. Details: ' + error.message);
+    });
+    
+
+export default app;
